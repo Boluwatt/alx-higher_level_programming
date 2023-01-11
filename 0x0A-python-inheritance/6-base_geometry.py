305a3335351b39d66369f44dfb904ef5566b7547
Boluwatife Oluwagbamila <69689649+Boluwatt@users.noreply.github.com>
2023-01-11 14:43:09 -0800

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 """Defines a base geometry class BaseGeometry."""
 
 
-
-
-
 class BaseGeometry:
+    """Represent base geometry."""
 
-        """Represent base geometry."""
-
-
-
-            def area(self):
-
-                        """Not implemented."""
-
-                                raise Exception("area() is not implemented")
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
